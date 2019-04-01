9319e1b3bb3da5a3904c60070ec8a66bc7198b33
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 16:46:29 +0800

    Create cache.js

---
+++ src/internal/operators/cache.js
@@ -0,0 +1,93 @@
+import AbortController from 'abort-controller';
+import Maybe from '../../maybe';
+import { cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const {
+    source, cached, observers, subscribed,
+  } = this;
+
+  if (!cached) {
+    const index = observers.length;
+    observers[index] = observer;
+
+    const controller = new AbortController();
+
+    controller.signal.addEventListener('abort', () => {
+      observers.splice(index, 1);
+    });
+
+    onSubscribe(controller);
+
+    if (!subscribed) {
+      source.subscribeWith({
+        onSubscribe() {
+          // not applicable
+        },
+        onSuccess: (x) => {
+          this.cached = true;
+          this.value = x;
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const obs of observers) {
+            obs.onSuccess(x);
+          }
+          this.observers = undefined;
+        },
+        onComplete: () => {
+          this.cached = true;
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const obs of observers) {
+            obs.onComplete();
+          }
+          this.observers = undefined;
+        },
+        onError: (x) => {
+          this.cached = true;
+          this.error = x;
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const obs of observers) {
+            obs.onError(x);
+          }
+          this.observers = undefined;
+        },
+      });
+      this.subscribed = true;
+    }
+  } else {
+    const controller = new AbortController();
+    onSubscribe(controller);
+
+    const { value, error } = this;
+    if (typeof value !== 'undefined') {
+      onSuccess(value);
+    } else if (typeof error !== 'undefined') {
+      onError(error);
+    } else {
+      onComplete();
+    }
+    controller.abort();
+  }
+}
+
+/**
+ * @ignore
+ */
+export default (source) => {
+  const maybe = new Maybe();
+  maybe.source = source;
+  maybe.cached = false;
+  maybe.subscribed = false;
+  maybe.observers = [];
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

a789648fdf29a53ced923dd403bcebd30aeefdca
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 17:58:06 +0800

    Create fromResolvable.js

---
+++ src/internal/operators/fromResolvable.js
@@ -0,0 +1,41 @@
+import AbortController from 'abort-controller';
+import { onErrorHandler, onSuccessHandler, onCompleteHandler } from '../utils';
+import Maybe from '../../maybe';
+import error from './error';
+
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = observer;
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  if (controller.signal.aborted) {
+    return;
+  }
+
+  this.controller = controller;
+  this.onComplete = onComplete;
+  this.onSuccess = onSuccess;
+  this.onError = onError;
+
+  const resolveNone = onCompleteHandler.bind(this);
+  const resolve = onSuccessHandler.bind(this);
+  const reject = onErrorHandler.bind(this);
+
+  this.subscriber(x => (x == null ? resolveNone() : resolve(x)), reject);
+}
+/**
+ * @ignore
+ */
+export default (subscriber) => {
+  if (typeof subscriber !== 'function') {
+    return error(new Error('Maybe.fromResolvable: expects a function.'));
+  }
+  const single = new Maybe();
+  single.subscriber = subscriber;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};

dc285148f202883f0b910a324c63ee72e32c1ca0
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 16:41:40 +0800

    Create defer.js

---
+++ src/internal/operators/defer.js
@@ -0,0 +1,43 @@
+import Maybe from '../../maybe';
+import { immediateError, cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  let result;
+
+  let err;
+  try {
+    result = this.supplier();
+    if (!(result instanceof Maybe)) {
+      throw new Error('Maybe.defer: supplier returned a non-Maybe.');
+    }
+  } catch (e) {
+    err = e;
+  }
+
+  if (typeof err !== 'undefined') {
+    immediateError(observer, err);
+  } else {
+    result.subscribeWith({
+      onSubscribe,
+      onComplete,
+      onSuccess,
+      onError,
+    });
+  }
+}
+/**
+ * @ignore
+ */
+export default (supplier) => {
+  const maybe = new Maybe();
+  maybe.supplier = supplier;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

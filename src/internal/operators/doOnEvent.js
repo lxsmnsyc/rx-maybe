7b132ce423636f85a3f71f605f698c28d66f546d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 17:43:48 +0800

    Create doOnEvent.js

---
+++ src/internal/operators/doOnEvent.js
@@ -0,0 +1,41 @@
+import Maybe from '../../maybe';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onSubscribe, onComplete, onSuccess, onError } = observer;
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe,
+    onComplete() {
+      callable();
+      onComplete();
+    },
+    onSuccess(x) {
+      callable(x);
+      onSuccess(x);
+    },
+    onError(x) {
+      callable(undefined, x);
+      onError(x);
+    },
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (typeof callable !== 'function') {
+    return source;
+  }
+
+  const single = new Maybe();
+  single.source = source;
+  single.callable = callable;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};

a6f87bfafd1774f5c7aa32aa0ca670f56858ba32
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 17:58:04 +0800

    Create fromPromise.js

---
+++ src/internal/operators/fromPromise.js
@@ -0,0 +1,48 @@
+import AbortController from 'abort-controller';
+import Maybe from '../../maybe';
+import {
+  isPromise, onSuccessHandler, onErrorHandler, onCompleteHandler,
+} from '../utils';
+import error from './error';
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = observer;
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  if (controller.signal.aborted) {
+    return;
+  }
+
+  this.controller = controller;
+  this.onComplete = onComplete;
+  this.onSuccess = onSuccess;
+  this.onError = onError;
+
+  const resolveNone = onCompleteHandler.bind(this);
+  const resolve = onSuccessHandler.bind(this);
+  const reject = onErrorHandler.bind(this);
+
+  this.promise.then(
+    x => (x == null ? resolveNone() : resolve(x)),
+    reject,
+  );
+}
+/**
+ * @ignore
+ */
+export default (promise) => {
+  if (!isPromise(promise)) {
+    return error(new Error('Maybe.fromPromise: expects a Promise-like value.'));
+  }
+  const single = new Maybe();
+  single.promise = promise;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};

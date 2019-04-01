ccab8b02dccf0d79ce6332b28b644ad3f9a3e96e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 17:42:42 +0800

    Create doOnError.js

---
+++ src/internal/operators/doOnError.js
@@ -0,0 +1,37 @@
+import Maybe from '../../maybe';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onComplete, onSuccess, onError,
+  } = observer;
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe,
+    onComplete,
+    onSuccess,
+    onError(x) {
+      callable(x);
+      onError(x);
+    },
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (typeof callable !== 'function') {
+    return source;
+  }
+
+  const single = new Maybe();
+  single.source = source;
+  single.callable = callable;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};

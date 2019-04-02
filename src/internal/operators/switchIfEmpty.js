94ea2a98205b2326bac28415c0e67c5d386fb09b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 10:36:46 +0800

    Create switchIfEmpty.js

---
+++ src/internal/operators/switchIfEmpty.js
@@ -0,0 +1,67 @@
+import AbortController from 'abort-controller';
+import { cleanObserver } from '../utils';
+import Maybe from '../../maybe';
+
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onSuccess, onComplete, onError,
+  } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  const { signal } = controller; 
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, other } = this;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onSuccess(x) {
+      onSuccess(x);
+      controller.abort();
+    },
+    onComplete() {
+      other.subscribeWith({
+        onSubscribe(ac) {
+          signal.addEventListener('abort', () => ac.abort());
+        },
+        onSuccess(x) {
+          onSuccess(x);
+          controller.abort();
+        },
+        onComplete() {
+          onComplete();
+          controller.abort();
+        },
+        onError(x) {
+          onError(x);
+          controller.abort();
+        },
+      });
+    },
+    onError(x) {
+      onError(x);
+      controller.abort();
+    },
+  });
+}
+
+export default (source, other) => {
+  if (!(other instanceof Maybe)) {
+    return source;
+  }
+
+  const maybe = new Maybe();
+  maybe.source = source;
+  maybe.other = other;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+
+  return maybe;
+};

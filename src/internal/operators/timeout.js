0cd32dc81aab088b39d55a20e3a8c631adaad18c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 08:29:30 +0800

    Create timeout.js

---
+++ src/internal/operators/timeout.js
@@ -0,0 +1,68 @@
+
+import AbortController from 'abort-controller';
+import Maybe from '../../maybe';
+import { cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const { amount } = this;
+
+  const controller = new AbortController();
+
+  const { signal } = controller;
+
+  onSubscribe(controller);
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const timeout = setTimeout(
+    () => {
+      onError(new Error('Maybe.timeout: TimeoutException (no success/completion signals within the specified timeout).'));
+      controller.abort();
+    },
+    amount,
+  );
+
+  signal.addEventListener('abort', () => {
+    clearTimeout(timeout);
+  });
+
+  this.source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onComplete() {
+      onComplete();
+      controller.abort();
+    },
+    onSuccess(x) {
+      onSuccess(x);
+      controller.abort();
+    },
+    onError(x) {
+      onError(x);
+      controller.abort();
+    },
+  });
+}
+/**
+ * @ignore
+ */
+export default (source, amount) => {
+  if (typeof amount !== 'number') {
+    return source;
+  }
+  const maybe = new Maybe();
+  maybe.source = source;
+  maybe.amount = amount;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

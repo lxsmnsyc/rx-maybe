f6f6d6566b1ed2554b8fca86de5a35c9c7ea3848
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 11:34:58 +0800

    Create filter.js

---
+++ src/internal/operators/filter.js
@@ -0,0 +1,70 @@
+import AbortController from 'abort-controller';
+import Maybe from '../../maybe';
+import { cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSubscribe, onSuccess, onComplete, onError,
+  } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, predicate } = this;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onComplete() {
+      onComplete();
+      controller.abort();
+    },
+    onSuccess(x) {
+      let result;
+
+      try {
+        result = predicate(x);
+      } catch (e) {
+        onError(e);
+        controller.abort();
+        return;
+      }
+
+      if (result) {
+        onSuccess(x);
+      } else {
+        onComplete();
+      }
+    },
+    onError(x) {
+      onError(x);
+      controller.abort();
+    },
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, predicate) => {
+  if (typeof predicate !== 'function') {
+    return source;
+  }
+
+  const maybe = new Maybe();
+  maybe.source = source;
+  maybe.predicate = predicate;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

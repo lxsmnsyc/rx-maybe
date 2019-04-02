6804fd835c8913b6a80262de438e00047e36e351
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 11:23:58 +0800

    Create operators.js

---
+++ src/internal/operators.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-cycle */
+/**
+ * @ignore
+ */
+export { default as amb } from './operators/amb';
+export { default as ambWith } from './operators/ambWith';
+export { default as cache } from './operators/cache';
+export { default as create } from './operators/create';
+export { default as complete } from './operators/complete';
+export { default as compose } from './operators/compose';
+export { default as defer } from './operators/defer';
+export { default as delay } from './operators/delay';
+export { default as delaySubscription } from './operators/delaySubscription';
+export { default as delayUntil } from './operators/delayUntil';
+export { default as doAfterSuccess } from './operators/doAfterSuccess';
+export { default as doAfterTerminate } from './operators/doAfterTerminate';
+export { default as doFinally } from './operators/doFinally';
+export { default as doOnAbort } from './operators/doOnAbort';
+export { default as doOnComplete } from './operators/doOnComplete';
+export { default as doOnError } from './operators/doOnError';
+export { default as doOnEvent } from './operators/doOnEvent';
+export { default as doOnSuccess } from './operators/doOnSuccess';
+export { default as doOnSubscribe } from './operators/doOnSubscribe';
+export { default as doOnTerminate } from './operators/doOnTerminate';
+export { default as error } from './operators/error';
+export { default as flatMap } from './operators/flatMap';
+export { default as fromCallable } from './operators/fromCallable';
+export { default as fromPromise } from './operators/fromPromise';
+export { default as fromResolvable } from './operators/fromResolvable';
+export { default as just } from './operators/just';
+export { default as lift } from './operators/lift';
+export { default as map } from './operators/map';
+export { default as merge } from './operators/merge';
+export { default as onErrorComplete } from './operators/onErrorComplete';
+export { default as onErrorResumeNext } from './operators/onErrorResumeNext';
+export { default as onErrorReturn } from './operators/onErrorReturn';
+export { default as onErrorReturnItem } from './operators/onErrorReturnItem';
+export { default as never } from './operators/never';
+export { default as retry } from './operators/retry';
+export { default as switchIfEmpty } from './operators/switchIfEmpty';
+export { default as takeUntil } from './operators/takeUntil';
+export { default as timer } from './operators/timer';
+export { default as timeout } from './operators/timeout';
+export { default as zip } from './operators/zip';
+export { default as zipWith } from './operators/zipWith';

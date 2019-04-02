70a036c56966cf6252e9e5466f89ef1b3e6af177
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 08:23:51 +0800

    Create just.js

---
+++ src/internal/operators/just.js
@@ -0,0 +1,22 @@
+import Maybe from '../../maybe';
+import error from './error';
+import { immediateSuccess } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  immediateSuccess(observer, this.value);
+}
+/**
+ * @ignore
+ */
+export default (value) => {
+  if (typeof value === 'undefined') {
+    return error(new Error('Maybe.just: received an undefined value.'));
+  }
+  const maybe = new Maybe();
+  maybe.value = value;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

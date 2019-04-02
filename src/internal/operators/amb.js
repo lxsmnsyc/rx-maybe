2a327dd9dd3227c3a9f782e7f6a8ddf1c42bd485
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-02 08:12:24 +0800

    Create amb.js

---
+++ src/internal/operators/amb.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-restricted-syntax */
+import AbortController from 'abort-controller';
+import Maybe from '../../maybe';
+import { isIterable, cleanObserver } from '../utils';
+import error from './error';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  const { signal } = controller;
+
+  onSubscribe(controller);
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { sources } = this;
+
+  for (const maybe of sources) {
+    if (signal.aborted) {
+      return;
+    }
+
+    if (maybe instanceof Maybe) {
+      maybe.subscribeWith({
+        onSubscribe(ac) {
+          signal.addEventListener('abort', () => ac.abort());
+        },
+        onComplete() {
+          onComplete();
+          controller.abort();
+        },
+        onSuccess(x) {
+          onSuccess(x);
+          controller.abort();
+        },
+        onError(x) {
+          onError(x);
+          controller.abort();
+        },
+      });
+    } else {
+      onError(new Error('Maybe.amb: One of the sources is a non-Maybe.'));
+      controller.abort();
+      break;
+    }
+  }
+}
+/**
+ * @ignore
+ */
+export default (sources) => {
+  if (!isIterable(sources)) {
+    return error(new Error('Maybe.amb: sources is not Iterable.'));
+  }
+  const maybe = new Maybe();
+  maybe.sources = sources;
+  maybe.subscribeActual = subscribeActual.bind(maybe);
+  return maybe;
+};

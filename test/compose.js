3c44bb87d5c4b33e102df48cc923ac17b33534aa
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 10:59:39 +0800

    Create compose.js

---
+++ test/compose.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Maybe from '../src/maybe';
+
+/**
+ *
+ */
+describe('Maybe', () => {
+  /**
+   *
+   */
+  describe('#compose', () => {
+    /**
+     *
+     */
+    it('should return the same instance if no function is provided', () => {
+      const source = Maybe.just('Hello');
+      const maybe = source.compose();
+
+      assert(source === maybe);
+    });
+    /**
+     *
+     */
+    it('should return a Maybe from the transformer', () => {
+      const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
+
+      assert(maybe instanceof Maybe);
+    });
+    /**
+     *
+     */
+    it('should correctly signal the composed Maybe', (done) => {
+      const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
+
+      maybe.subscribe(
+        x => (x === 'Hello World' ? done() : done(x)),
+        () => done(false),
+        () => done(false),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if the transformer function returned a non-Maybe', (done) => {
+      const maybe = Maybe.just('Hello').compose(() => undefined);
+
+      maybe.subscribe(
+        () => done(false),
+        () => done(false),
+        () => done(),
+      );
+    });
+  });
+});

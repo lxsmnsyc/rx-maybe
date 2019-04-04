f2c899336b9aefb7c2ead683a79f6a29115c96a2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 15:40:43 +0800

    Update create.js

--- test/create.js
+++ test/create.js
@@ -5,86 +5,81 @@ import Maybe from '../src/maybe';
 /**
  *
  */
-describe('Maybe', () => {
+describe('#create', () => {
   /**
    *
    */
-  describe('#create', () => {
-    /**
-     *
-     */
-    it('should create a Maybe', () => {
-      const maybe = Maybe.create(e => e.onSuccess('Hello World'));
+  it('should create a Maybe', () => {
+    const maybe = Maybe.create(e => e.onSuccess('Hello World'));
 
-      assert(maybe instanceof Maybe);
-    });
-    /**
-     *
-     */
-    it('should signal error if the create received a non-function', (done) => {
-      const maybe = Maybe.create();
+    assert(maybe instanceof Maybe);
+  });
+  /**
+   *
+   */
+  it('should signal error if the create received a non-function', (done) => {
+    const maybe = Maybe.create();
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the emitter signals success with undefined value.', (done) => {
-      const maybe = Maybe.create(e => e.onSuccess() || e.onSuccess());
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the emitter signals success with undefined value.', (done) => {
+    const maybe = Maybe.create(e => e.onSuccess() || e.onSuccess());
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the emitter signals error with undefined value.', (done) => {
-      const maybe = Maybe.create(e => e.onError() || e.onError());
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the emitter signals error with undefined value.', (done) => {
+    const maybe = Maybe.create(e => e.onError() || e.onError());
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should be aborted successfully if emitter is aborted before any signal.', (done) => {
+    const maybe = Maybe.create((e) => {
+      setTimeout(e.onSuccess, 100, true);
+      e.abort();
     });
-    /**
-     *
-     */
-    it('should be aborted successfully if emitter is aborted before any signal.', (done) => {
-      const maybe = Maybe.create((e) => {
-        setTimeout(e.onSuccess, 100, true);
-        e.abort();
-      });
 
-      const controller = maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(false),
-      );
-      if (controller.signal.aborted) {
-        done();
-      }
+    const controller = maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(false),
+    );
+    if (controller.signal.aborted) {
+      done();
+    }
+  });
+  /**
+   *
+   */
+  it('should signal error if subscriber throws an error.', (done) => {
+    const maybe = Maybe.create(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should signal error if subscriber throws an error.', (done) => {
-      const maybe = Maybe.create(() => {
-        throw new Error('Expected');
-      });
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
-    });
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
   });
 });

d9433af33ad0d403fa42b2c20084ec89eda31d83
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 15:40:09 +0800

    Update compose.js

--- test/compose.js
+++ test/compose.js
@@ -5,51 +5,46 @@ import Maybe from '../src/maybe';
 /**
  *
  */
-describe('Maybe', () => {
+describe('#compose', () => {
   /**
    *
    */
-  describe('#compose', () => {
-    /**
-     *
-     */
-    it('should return the same instance if no function is provided', () => {
-      const source = Maybe.just('Hello');
-      const maybe = source.compose();
+  it('should return the same instance if no function is provided', () => {
+    const source = Maybe.just('Hello');
+    const maybe = source.compose();
 
-      assert(source === maybe);
-    });
-    /**
-     *
-     */
-    it('should return a Maybe from the transformer', () => {
-      const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
+    assert(source === maybe);
+  });
+  /**
+   *
+   */
+  it('should return a Maybe from the transformer', () => {
+    const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
 
-      assert(maybe instanceof Maybe);
-    });
-    /**
-     *
-     */
-    it('should correctly signal the composed Maybe', (done) => {
-      const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
+    assert(maybe instanceof Maybe);
+  });
+  /**
+   *
+   */
+  it('should correctly signal the composed Maybe', (done) => {
+    const maybe = Maybe.just('Hello').compose(source => source.map(x => `${x} World`));
 
-      maybe.subscribe(
-        x => (x === 'Hello World' ? done() : done(x)),
-        () => done(false),
-        () => done(false),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the transformer function returned a non-Maybe', (done) => {
-      const maybe = Maybe.just('Hello').compose(() => undefined);
+    maybe.subscribe(
+      x => (x === 'Hello World' ? done() : done(x)),
+      () => done(false),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the transformer function returned a non-Maybe', (done) => {
+    const maybe = Maybe.just('Hello').compose(() => undefined);
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
-    });
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
   });
 });

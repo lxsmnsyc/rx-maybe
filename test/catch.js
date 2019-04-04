fd2c5fafbd84047e9cf65dc6d76abe3e8c87daf2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 10:59:37 +0800

    Create catch.js

---
+++ test/catch.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Maybe from '../src/maybe';
+
+/**
+ *
+ */
+describe('Maybe', () => {
+  /**
+   *
+   */
+  describe('#catch', () => {
+    /**
+     *
+     */
+    it('should create a Promise', () => {
+      const maybe = Maybe.just('Hello').catch(x => x);
+      assert(maybe instanceof Promise);
+    });
+  });
+});

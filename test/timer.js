eaa99f40fd62471d48a9b562381f825208434ced
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 16:07:08 +0800

    Create timer.js

---
+++ test/timer.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Maybe from '../src/maybe';
+
+/**
+ *
+ */
+describe('#timer', () => {
+  /**
+   *
+   */
+  it('should create a Maybe', () => {
+    const maybe = Maybe.timer(100);
+    assert(maybe instanceof Maybe);
+  });
+  /**
+   *
+   */
+  it('should signal error if amount is not a number.', (done) => {
+    const maybe = Maybe.timer();
+
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal success 0', (done) => {
+    const maybe = Maybe.timer(100);
+    maybe.subscribe(
+      x => (x === 0 ? done() : done(false)),
+      () => done(false),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should not signal success if aborted.', (done) => {
+    const maybe = Maybe.timer(100);
+    const controller = maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(false),
+    );
+
+
+    controller.abort();
+    if (controller.signal.aborted) {
+      done();
+    }
+  });
+});

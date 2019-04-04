c0e8273c33ae5a66f274137220483c8b8a0e6d76
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 16:01:12 +0800

    Create lift.js

---
+++ test/lift.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Maybe from '../src/maybe';
+/**
+ *
+ */
+describe('Maybe', () => {
+  /**
+   *
+   */
+  describe('#lift', () => {
+    /**
+     *
+     */
+    it('should return the same instance if no function is provided', () => {
+      const source = Maybe.just('Hello');
+      const maybe = source.lift();
+
+      assert(source === maybe);
+    });
+    /**
+     *
+     */
+    it('should signal an error if the lift operator returned a non-Observer', (done) => {
+      const maybe = Maybe.just('Hello').lift(() => {});
+
+      maybe.subscribe(
+        () => done(false),
+        () => done(false),
+        () => done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should subscribe successfully', (done) => {
+      const maybe = Maybe.just('Hello').lift(observer => ({ onSubscribe: observer.onSubscribe, onSuccess: observer.onSuccess }));
+
+      maybe.subscribe(
+        () => done(),
+        () => done(false),
+        () => done(false),
+      );
+    });
+  });
+});

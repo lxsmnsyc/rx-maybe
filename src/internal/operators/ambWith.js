77885d34902f18b484afc139a2cff5e7ec55d7b5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 14:44:47 +0800

    Update ambWith.js

--- src/internal/operators/ambWith.js
+++ src/internal/operators/ambWith.js
@@ -1,68 +1,5 @@
-import AbortController from 'abort-controller';
 import Maybe from '../../maybe';
-import { cleanObserver } from '../utils';
-
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const {
-    onSuccess, onComplete, onError, onSubscribe,
-  } = cleanObserver(observer);
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-
-  const sharedComplete = () => {
-    if (!signal.aborted) {
-      onComplete();
-      controller.abort();
-    }
-  };
-  const sharedSuccess = (x) => {
-    if (!signal.aborted) {
-      onSuccess(x);
-      controller.abort();
-    }
-  };
-  const sharedError = (x) => {
-    if (!signal.aborted) {
-      onError(x);
-      controller.abort();
-    }
-  };
-
-  const { source, other } = this;
-
-  source.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onComplete: sharedComplete,
-    onSuccess: sharedSuccess,
-    onError: sharedError,
-  });
-  other.subscribeWith({
-    onSubscribe(ac) {
-      if (signal.aborted) {
-        ac.abort();
-      } else {
-        signal.addEventListener('abort', () => ac.abort());
-      }
-    },
-    onComplete: sharedComplete,
-    onSuccess: sharedSuccess,
-    onError: sharedError,
-  });
-}
+import amb from './amb';
 /**
  * @ignore
  */
@@ -70,8 +7,5 @@ export default (source, other) => {
   if (!(other instanceof Maybe)) {
     return source;
   }
-  const maybe = new Maybe(subscribeActual);
-  maybe.source = source;
-  maybe.other = other;
-  return maybe;
+  return amb([source, other]);
 };

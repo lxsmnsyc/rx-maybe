fdf57890627982c0a39d81802e2102edc7d779f4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 14:45:08 +0800

    Create doOnCancel.js

---
+++ src/internal/operators/doOnCancel.js
@@ -0,0 +1,36 @@
+import Maybe from '../../maybe';
+import { cleanObserver, isFunction } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const {
+    onSuccess, onComplete, onError, onSubscribe,
+  } = cleanObserver(observer);
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      ac.addEventListener('cancel', callable);
+      onSubscribe(ac);
+    },
+    onComplete,
+    onSuccess,
+    onError,
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (!isFunction(callable)) {
+    return source;
+  }
+  const maybe = new Maybe(subscribeActual);
+  maybe.source = source;
+  maybe.callable = callable;
+  return maybe;
+};

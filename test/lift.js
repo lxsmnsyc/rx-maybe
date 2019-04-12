ccf66fcd4cbbc7ecc7e4b26f66f10eeeb57bc08e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 14:46:54 +0800

    Update lift.js

--- test/lift.js
+++ test/lift.js
@@ -4,43 +4,38 @@ import Maybe from '../src/maybe';
 /**
  *
  */
-describe('Maybe', () => {
+describe('#lift', () => {
   /**
    *
    */
-  describe('#lift', () => {
-    /**
-     *
-     */
-    it('should return the same instance if no function is provided', () => {
-      const source = Maybe.just('Hello');
-      const maybe = source.lift();
+  it('should return the same instance if no function is provided', () => {
+    const source = Maybe.just('Hello');
+    const maybe = source.lift();
 
-      assert(source === maybe);
-    });
-    /**
-     *
-     */
-    it('should signal an error if the lift operator returned a non-Observer', (done) => {
-      const maybe = Maybe.just('Hello').lift(() => {});
+    assert(source === maybe);
+  });
+  /**
+   *
+   */
+  it('should signal an error if the lift operator returned a non-Observer', (done) => {
+    const maybe = Maybe.just('Hello').lift(() => {});
 
-      maybe.subscribe(
-        () => done(false),
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should subscribe successfully', (done) => {
-      const maybe = Maybe.just('Hello').lift(observer => ({ onSubscribe: observer.onSubscribe, onSuccess: observer.onSuccess }));
+    maybe.subscribe(
+      () => done(false),
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should subscribe successfully', (done) => {
+    const maybe = Maybe.just('Hello').lift(observer => ({ onSubscribe: observer.onSubscribe, onSuccess: observer.onSuccess }));
 
-      maybe.subscribe(
-        () => done(),
-        () => done(false),
-        () => done(false),
-      );
-    });
+    maybe.subscribe(
+      () => done(),
+      () => done(false),
+      () => done(false),
+    );
   });
 });

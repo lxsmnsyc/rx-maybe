57c8aac21168dc7aea1131fc374bab7ec2102b9e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-24 09:08:38 +0800

    Update empty.js

--- src/internal/operators/empty.js
+++ src/internal/operators/empty.js
@@ -1,20 +1,13 @@
 import Maybe from '../../maybe';
 import { immediateComplete } from '../utils';
 
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  immediateComplete(observer);
-}
-
 let INSTANCE;
 /**
  * @ignore
  */
 export default () => {
   if (typeof INSTANCE === 'undefined') {
-    INSTANCE = new Maybe(subscribeActual);
+    INSTANCE = new Maybe(observer => immediateComplete(observer));
   }
   return INSTANCE;
 };

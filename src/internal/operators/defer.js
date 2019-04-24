5137992c5ebfed1cdb21f71416571abb1ed02015
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-24 09:08:29 +0800

    Update defer.js

--- src/internal/operators/defer.js
+++ src/internal/operators/defer.js
@@ -1,5 +1,6 @@
 import Maybe from '../../maybe';
-import { immediateError, cleanObserver } from '../utils';
+import { immediateError, cleanObserver, exists } from '../utils';
+import is from '../is';
 
 /**
  * @ignore
@@ -14,14 +15,14 @@ function subscribeActual(observer) {
   let err;
   try {
     result = this.supplier();
-    if (!(result instanceof Maybe)) {
+    if (!is(result)) {
       throw new Error('Maybe.defer: supplier returned a non-Maybe.');
     }
   } catch (e) {
     err = e;
   }
 
-  if (err != null) {
+  if (exists(err)) {
     immediateError(observer, err);
   } else {
     result.subscribeWith({
